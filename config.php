3dbf051058804a2f1618cceeff0e353b7eb47bae
NLremix <68206455+NLremix@users.noreply.github.com>
2020-07-13 06:57:36 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+# config by : NLremix_ 
+# channel : Nindi Luthfiyyah 
+# telegram : @NLremix_ & @NL2020
+
+$auth = "BASIC Ojc5YjM4ODgzMTU2OTZjYjc2Nzk2YjUwODM0Mzk4MDg0";
+$ts = "1594357632245";
+$index = "16";
+$cookie = "u=61544736;n=0000000025a7be3a4e97d5524e97d552";
+
+?>
